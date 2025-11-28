05011de27b375dd91db7834b69abdd0701b55f85
George Kadianakis <desnacked@riseup.net>
2025-11-28 14:28:49 +0200

    Add all the remaining ZisK circuits as well
    See https://github.com/ethereum/soundcalc/issues/18

--- soundcalc/zkvms/zisk.py
+++ soundcalc/zkvms/zisk.py
@@ -52,11 +52,46 @@ def _make_circuit(name, bits, bf, d, fixed, stage1, pols, queries, opens, fri_fa
 # Base ZisK circuits
 # Columns: (name, bits, bf, d, fixed, stage1, pols, queries, opens, fri_fa)
 #
+# See https://github.com/ethereum/soundcalc/issues/18 for more details:
+#
+#
 # Aligned for better reading:
-#                              bits bf  d  fix stg1  pols  qry opn  fri_fa
+#                              bits bf  d  fix  stg1  pols  qry opn  fri_fa
 ZISK_BASE_CIRCUITS = [
-    ("Main",                    22,  1, 3,   3,  38,   61, 128,  3, "23-19-15-11-8-5"),
-    ("Rom",                     22,  1, 2,   1,   1,   18, 128,  3, "23-19-15-11-8-5"),
+    ("Main",                    22,  1, 3,   3,   38,   61, 128,  3, "23-19-15-11-8-5"),
+    ("Rom",                     22,  1, 2,   1,    1,   18, 128,  3, "23-19-15-11-8-5"),
+    ("Mem",                     22,  1, 3,   2,   13,   29, 128,  3, "23-19-15-11-8-5"),
+    ("RomData",                 21,  1, 3,   2,    6,   19, 128,  3, "22-18-14-11-8-5"),
+    ("InputData",               21,  1, 3,   2,    9,   27, 128,  3, "22-18-14-11-8-5"),
+    ("MemAlign",                21,  1, 3,   2,   29,   59, 128,  3, "22-18-14-11-8-5"),
+    ("MemAlignByte",            22,  1, 3,   1,   16,   25, 128,  3, "23-19-15-11-8-5"),
+    ("MemAlignReadByte",        22,  1, 3,   1,   10,   18, 128,  3, "23-19-15-11-8-5"),
+    ("MemAlignWriteByte",       22,  1, 3,   1,   14,   23, 128,  3, "23-19-15-11-8-5"),
+    ("Arith",                   21,  1, 3,   1,   44,   64, 128,  3, "22-18-14-11-8-5"),
+    ("Binary",                  22,  1, 3,   1,   39,   49, 128,  3, "23-19-15-11-8-5"),
+    ("BinaryAdd",               22,  1, 3,   1,   10,   18, 128,  3, "23-19-15-11-8-5"),
+    ("BinaryExtension",         22,  1, 3,   1,   29,   40, 128,  3, "23-19-15-11-8-5"),
+    ("Add256",                  20,  1, 3,   1,   47,   69, 128,  3, "21-17-13-9-5"),
+    ("ArithEq",                 20,  1, 3,   2,   39,  434, 128, 36, "21-17-13-9-5"),
+    ("ArithEq384",              20,  1, 3,   2,   33,  536, 128, 54, "21-17-13-9-5"),
+    ("Keccakf",                 16,  1, 3,   2, 2137, 4065, 128, 26, "17-13-9-5"),
+    ("Sha256f",                 18,  1, 3,   2,  102, 1265, 128, 87, "19-15-11-8-5"),
+    ("SpecifiedRanges",         20,  1, 3,  34,   33,   88, 128,  3, "21-17-13-9-5"),
+    ("VirtualTable0",           20,  1, 3, 100,   16,  129, 128,  3, "21-17-13-9-5"),
+    ("VirtualTable1",           20,  1, 3, 145,   16,  174, 128,  3, "21-17-13-9-5"),
+]
+
+# Recursive Proving Circuits
+#                              bits bf  d  fix  stg1  pols  qry opn  fri_fa
+ZISK_RECURSIVE_CIRCUITS = [
+    ("ArithEq Compressor",      18,  2, 3,  45,   36,  238,  64,  4, "20-16-12-8-5"),
+    ("ArithEq384 Compressor",   18,  2, 3,  45,   36,  238,  64,  4, "20-16-12-8-5"),
+    ("Keccakf Compressor",      21,  2, 3,  45,   36,  238,  64,  4, "23-19-15-11-8-5"),
+    ("Sha256f Compressor",      19,  2, 3,  45,   36,  238,  64,  4, "21-17-13-9-5"),
+    ("VirtualTable1 Compressor",18,  2, 3,  45,   36,  238,  64,  4, "20-16-12-8-5"),
+    ("Recursive1",              17,  3, 3,  45,   36,  243,  43,  4, "20-16-12-8-5"),
+    ("Recursive2",              17,  3, 3,  45,   36,  243,  43,  4, "20-16-12-8-5"),
+    ("Final",                   16,  4, 3,  45,   42,  249,  32,  4, "20-15-10"),
 ]
 
 
@@ -66,13 +101,11 @@ class ZiskPreset:
         """
         Create a ZisK VM with multiple circuits.
 
-        For ZisK, we populate the trace parameters from its constraint description:
-            https://github.com/0xPolygonHermez/zisk/blob/main/pil/zisk.pil
-
-        The rest of the parameters are adapted from the "eSTARK: Extending STARKs with Arguments" paper:
-           https://eprint.iacr.org/2023/474
+        For ZisK, we populate the parameters from the following data:
+        https://github.com/ethereum/soundcalc/issues/18
         """
         circuits = [_make_circuit(*row) for row in ZISK_BASE_CIRCUITS]
+        circuits += [_make_circuit(*row) for row in ZISK_RECURSIVE_CIRCUITS]
         return FRIBasedVM(name="ZisK", circuits=circuits)
 
 
